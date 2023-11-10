32f8ca754c9766227a9bfdf5a942a49d2f0de6d6
Rifqi Favian <piyanh90@gmail.com>
2023-11-10 17:38:07 +0700

    updating(2) 10 nov

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
   base: "/new---portfolio-project/",
+  plugins: [react()],
 });
