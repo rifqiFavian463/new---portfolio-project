c175c32e58498f5684d160183db5d2494f8d6f0b
Rifqi Favian <piyanh90@gmail.com>
2023-11-10 17:44:45 +0700

    updating(3) 10 nov

--- src/AnimRoutes.jsx
+++ src/AnimRoutes.jsx
@@ -11,10 +11,10 @@ function AnimRoutes() {
   return (
     <AnimatePresence initial={true} mode="wait">
       <Routes key={location.pathname} location={location}>
-        <Route path="/" element={<Profile />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/academy" element={<Academy />} />
-        <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="/new---portfolio-project/" element={<Profile />} />
+        <Route path="/new---portfolio-project/about" element={<About />} />
+        <Route path="/new---portfolio-project/academy" element={<Academy />} />
+        <Route path="/new---portfolio-project/portfolio" element={<Portfolio />} />
       </Routes>
     </AnimatePresence>
   );
